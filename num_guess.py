0796f9c23d3ca1f3e132f153641415267cd3c842
Delainee Lenss <lenssd@oregonstate.edu>
2022-10-11 21:49:18 -0500

    Commit Message

--- num_guess.py
+++ num_guess.py
@@ -6,16 +6,16 @@
 # If the user's guess is lower than the target integer, the program should display "too low"
 # Then the program should print how many guesses it took.
 
-user = int(input("Enter the integer for the player to guess."))
-guess = int(input("Enter your guess."))
+user = int(input("Enter the integer for the player to guess.\n"))
+guess = int(input("Enter your guess.\n"))
 num = 0
 
 while True:  # while loop will continue until the guess is correct.
     num = num + 1  # num will count the number of attempts.
     if guess < user:
-        guess = int(input("Too low - try again:"))
+        guess = int(input("Too low - try again:\n"))
     elif guess > user:
-        guess = int(input('Too high - try again:'))
+        guess = int(input("Too high - try again:\n"))
     else:
         break  # break will exit the cycle
 
